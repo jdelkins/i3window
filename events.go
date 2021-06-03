efe4db2dacdfe5707b7c71b9fe813b796d7e009b
Joel D. Elkins <joel@elkins.co>
2021-06-03 17:34:25 -0500

    Check if my visible workspace is empty before clearing the message

--- events.go
+++ events.go
@@ -19,17 +19,45 @@ func nodeToWorkspace(ipc *i3ipc.IPCSocket, n *i3ipc.I3Node) *i3ipc.Workspace {
 	return nil
 }
 
-func showFocusedName() {
-	ipc, _ := i3ipc.GetIPCSocket()
+func workspaceToNode(ipc *i3ipc.IPCSocket, ws *i3ipc.Workspace) *i3ipc.I3Node {
+	root, _ := ipc.GetTree()
+	for _, n := range root.Descendents() {
+		if n.Type == "workspace" && n.Name == ws.Name {
+			return n
+		}
+	}
+	return nil
+}
+
+func myWorkspace(ipc *i3ipc.IPCSocket) *i3ipc.Workspace {
+	wss, _ := ipc.GetWorkspaces()
+	for _, w := range wss {
+		if w.Output == output && (w.Visible || w.Focused) {
+			return &w
+		}
+	}
+	return nil
+}
+
+func wsCons(ipc *i3ipc.IPCSocket, ws *i3ipc.Workspace) []*i3ipc.I3Node {
+	mynode := workspaceToNode(ipc, ws)
+	return mynode.Descendents()
+}
+
+func showFocusedName(ipc *i3ipc.IPCSocket) {
 	tree, _ := ipc.GetTree()
 	focused := tree.FindFocused()
 	if focused == nil {
-		fmt.Println("")
+		if len(wsCons(ipc, myWorkspace(ipc))) == 0 {
+			fmt.Println("")
+		}
 		return
 	}
 	wsn := focused.Workspace()
 	if wsn == nil {
-		fmt.Println("")
+		if len(wsCons(ipc, myWorkspace(ipc))) == 0 {
+			fmt.Println("")
+		}
 		return
 	}
 	ws := nodeToWorkspace(ipc, wsn)
@@ -41,14 +69,19 @@ func showFocusedName() {
 func main() {
 	output = os.Args[1]
 	i3ipc.StartEventListener()
+	ipc, err := i3ipc.GetIPCSocket()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Could not get i3 IPC socket")
+		os.Exit(1)
+	}
 	w_events, _ := i3ipc.Subscribe(i3ipc.I3WindowEvent)
 	ws_events, _ := i3ipc.Subscribe(i3ipc.I3WorkspaceEvent)
 	for {
 		select {
 		case <-w_events:
-			showFocusedName()
+			showFocusedName(ipc)
 		case <-ws_events:
-			showFocusedName()
+			showFocusedName(ipc)
 		}
 	}
 }
